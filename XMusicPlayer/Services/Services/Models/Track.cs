bf5c8e454ff0408ab9b6e073ff7df734dc4afa9f
Daniele Bruni <zalinasco@outlook.com>
2018-01-30 12:11:06 +0100

    Initial Commit

---
+++ XMusicPlayer/Services/Services/Models/Track.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Services.Models
+{
+    public class Track
+    {
+        public string filepath
+        {
+            get; set; // proprietà implicita
+        }
+
+        public string title
+        {
+            get; set; // proprietà implicita
+        }
+
+        public string album
+        {
+            get; set; // proprietà implicita
+        }
+
+        public string tnumber
+        {
+            get; set; // proprietà implicita
+        }
+
+        public int ttime
+        {
+            get; set; // proprietà implicita
+        }
+
+
+    }
+
+
+}
